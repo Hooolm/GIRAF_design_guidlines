60ad7eb7ecbeed6e79f42f18049cccb34e285f71
Hooolm <r.holm.j93@gmail.com>
2015-05-19 19:16:07 +0200

    Update appendix regarding GirafButton

--- content/appendix/usage_of_girafbuttons/ExampleActivity.java
+++ content/appendix/usage_of_girafbuttons/ExampleActivity.java
@@ -10,12 +10,12 @@ public class ExampleActivity extends GirafActivity {
         setContentView(R.layout.example_activity);
 
         // Get a button from xml
-        GirafButton xmlButton = (GirafButton) findViewById(R.id.giraf_button);
+        GirafButton xmlButton = (GirafButton) findViewById(R.id.giraf_button); //* \label{line:button_java:findviewbyid}
 
         // Program buttons directly in code
-        GirafButton iconButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_camera));
-        GirafButton textButton = new GirafButton(this, "Camera");
-        GirafButton iconTextButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_camera), "Camera");
+        GirafButton textButton = new GirafButton(this, "Camera"); //* \label{line:button_java:constructorstart}
+        GirafButton iconButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_camera)); 
+        GirafButton iconTextButton = new GirafButton(this, getResources().getDrawable(R.drawable.icon_camera), "Camera"); //* \label{line:button_java:constructorend}
 
     }
 }
